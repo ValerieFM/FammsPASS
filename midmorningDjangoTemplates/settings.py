5dd4972da593d7e1a85f3ff91f76c78234319184
ValerieFM <valeriembaka0@gmail.com>
2023-03-10 12:13:31 +0300

    second commit

--- midmorningDjangoTemplates/settings.py
+++ midmorningDjangoTemplates/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'bootstrap4',
+    'django_daraja',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +125,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
